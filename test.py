a8e4c8fd95cda22474d9d3ae0160589bea4a99e0
Jason-8577 <jasono119@nycstudents.net>
2026-01-06 09:25:00 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,31 @@
+class BOSS:
+    def __init__(self, atk, range, hp):
+        self.atk = atk
+        self.range = range
+        self.hp = hp
+
+    def attack(self):
+        self.atk == 50
+        print(f"{self.name} chanted {Game_Input}")
+
+    def buff(self):
+        if self.hp <= 200:
+            self.atk += 50
+            print("Damage Triggered.")
+    
+    def health_point(self):
+        if self.hp >= 0 and self.hp <= 50:
+            self.hp += 100 
+            print(f"{self.name} triggered Rampage")
+
+Morph = BOSS("Morph", 50, 50, 500)
+
+while True:
+
+    print("1: attack")
+
+    INPUTS = int(input("Enter an option:"))
+
+    if I == 1:
+        Game_Input = input("Input a spell:")
+        Morph.atk()
