23c061c4a90cb2e0c979412ba189816c6a2ad2e9
Jason-8577 <jasono119@nycstudents.net>
2026-01-07 08:48:35 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 class BOSS:
-    def __init__(self, atk, range, hp):
+    def __init__(self, name, atk, range, hp):
         self.atk = atk
+        self.name = name
         self.range = range
         self.hp = hp
 
     def attack(self):
         self.atk == 50
         print(f"{self.name} chanted {Game_Input}")
+        print(f"Enemy took {self.atk} dmg.")
 
     def buff(self):
         if self.hp <= 200:
@@ -18,14 +20,18 @@ class BOSS:
             self.hp += 100 
             print(f"{self.name} triggered Rampage")
 
+    def health_death(self):
+        if self.hp >= 0:
+            print(f"{self.name} has died...")
+
 Morph = BOSS("Morph", 50, 50, 500)
 
 while True:
 
     print("1: attack")
 
-    Enter = int(input("Enter an option:"))
+    Inputs_input = int(input("Enter an option:"))
 
-    if Enter == 1:
+    if Inputs_input == 1:
         Game_Input = input("Input a spell:")
-        Morph.atk()
+        Morph.attack()
