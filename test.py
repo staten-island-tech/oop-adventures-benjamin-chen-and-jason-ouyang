f5b4a99c83d7ecef9d56b0905168589dff625f6d
Jason-8577 <jasono119@nycstudents.net>
2026-01-08 09:06:54 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,38 +1,45 @@
 class wizard:
-    def __init__(self, name, atk, defense, range, health):
+    def __init__(self, name, atk, health):
         self.name = name
         self.atk = atk
-        self.defense = defense
-        self.range = range
         self.health = health
 
     def attack(self):
-        self.atk == 35
-        self.range == 25
-        self.defense -= 5
-        print(f"{self.name} chanted {user_input}.")
-        print(f"{self.name} inflicted {self.atk} damage.")
-    
-    def defensive_spell(self):
-        self.atk += 0
-        self.range -= 15
-        self.defense += 35
+        if self.health <= 25 and self.health >= 2:
+            print(f"{self.name} inflicted {self.atk*2}: [DOUBLE DMG]")
+        elif self.health >= 50:
+            self.atk == 25
+            print(f"{self.name} unleased a {user_input}: INFLICTED {self.atk} dmg.")
+        elif self.health == 0:
+            print(f"{self.name} activated self destruct:{self.atk*4} dmg:")
 
-    def range_buff(self):
-        self.atk -= 34
-        self.range += 100
-        self.defense -= 10
+    def Status(self):
+        print(f"Name: {self.name}")
+        print(f"Attack [DMG]: {self.atk}, {self.atk*2}, {self.atk*4}")
+        print(f"Health: {self.health}")
+        
 
-Wizard = wizard("Wizard", 35, 15, 25, 100)
+Wizard = wizard("Wizard", 25, 50)
 
 while True:
 
     print("1: atk")
-    print("2: defense")
-    print("3: buff")
+    print("2: Status")
 
-    Inputs = int(input("Input 1, 2, or 3:"))
+    Inputs = int(input("Input an option:"))
 
     if Inputs == 1:
         user_input = input("Input a spell.")
-        Wizard.attack()
+        if Wizard.health > 0:
+            Wizard.attack()
+        if Wizard.health == 0:
+            print("Would you like to...?")
+            print("1: Yes")
+            print("2: Yes")
+            Game_input = int(input("Input a number:"))
+            if Game_input == 1 or 2:
+                Wizard.attack()
+                print("Wizard has died : Game Restart :")
+                break
+    elif Inputs == 2: 
+        Wizard.Status()
